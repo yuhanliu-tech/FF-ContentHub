42a156ef3586fd416658252b7bf1e96aa59ec239
Yuhan Liu <42754447+yuhanliu-tech@users.noreply.github.com>
2025-12-10 13:17:39 -0500

    sqlite to postgres db

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,20 +1,3 @@
-import type { Schema, Struct } from '@strapi/strapi';
-
-export interface ContentPageContent extends Struct.ComponentSchema {
-  collectionName: 'components_content_page_contents';
-  info: {
-    displayName: 'page-content';
-    icon: 'file';
-  };
-  attributes: {
-    words: Schema.Attribute.RichText;
-  };
-}
-
-declare module '@strapi/strapi' {
-  export module Public {
-    export interface ComponentSchemas {
-      'content.page-content': ContentPageContent;
-    }
-  }
-}
+/*
+ * The app doesn't have any components yet.
+ */
